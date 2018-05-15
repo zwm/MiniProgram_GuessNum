8c8e709f5c0cdecd04e030843587a36d046fad3f
zwm <mingforregister@163.com>
2018-05-15 21:32:57 +0800

    Study App(),Page()

---
+++ Document/Study/IEMiniProgramHelp.bat
@@ -0,0 +1,6 @@
+﻿@echo off
+for /f "tokens=3,4" %%a in ('"reg query HKEY_CLASSES_ROOT\http\shell\open\command"') do (set SoftWareRoot=%%a %%b)
+::输入地址
+set url=https://developers.weixin.qq.com/miniprogram/dev/index.html
+::打开网页，之后进行后续操作
+start % SoftWareRoot % %url%
--- Source/app.js
+++ Source/app.js
@@ -9,18 +9,28 @@ App({
     // 登录
     wx.login({
       success: res => {
+        console.log("LINE1    login res.code    : "+res.code)
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
       }
     })
     // 获取用户信息
     wx.getSetting({
       success: res => {
+        console.log("LINE3    userInfo          : "+res.authSetting['scope.userInfo'])
+        console.log("LINE4    userLocation      : "+res.authSetting['scope.userLocation'])
+        console.log("LINE5    address           : "+res.authSetting['scope.address'])
+        console.log("LINE6    invoiceTitle      : "+res.authSetting['scope.invoiceTitle'])
+        console.log("LINE7    werun             : "+res.authSetting['scope.werun'])
+        console.log("LINE8    record            : "+res.authSetting['scope.record'])
+        console.log("LINE9    writePhotosAlbum  : "+res.authSetting['scope.writePhotosAlbum'])
+        console.log("LINE10   camera            : "+res.authSetting['scope.camera'])
+//        /*
         if (res.authSetting['scope.userInfo']) {
           // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
           wx.getUserInfo({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+//              this.globalData.userInfo = res.userInfo
 
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
@@ -30,6 +40,7 @@ App({
             }
           })
         }
+//        */
       }
     })
   },
